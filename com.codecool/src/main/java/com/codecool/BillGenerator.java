b1940e844c7f41a662f08bef9b580a7057ce60f1
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-11-28 14:55:01 +0100

    Create Bill generator

--- com.codecool/src/main/java/com/codecool/BillGenerator.java
+++ com.codecool/src/main/java/com/codecool/BillGenerator.java
@@ -8,12 +8,27 @@ import java.util.Map;
 
 public class BillGenerator {
     Map<String, Map<Integer, Float>> discountMap;
-    String fileFromCashDesk;
+    Map<String, Integer> billMap;
 
 
     public BillGenerator(String fileWithDiscount, String fileFromCashDesk) {
         this.discountMap = readDiscountFromFile(fileWithDiscount);
-        this.fileFromCashDesk = fileFromCashDesk;
+        this.billMap = sumBillQuantity(fileFromCashDesk);
+    }
+
+    private Map<String,Integer> sumBillQuantity(String fileFromCashDesk) {
+        Map<String,Integer> billMap = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileFromCashDesk))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                billMap.putIfAbsent(line, Integer.valueOf(0));
+                billMap.put(line, billMap.get(line) + 1);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return billMap;
     }
 
     private Map<String,Map<Integer,Float>> readDiscountFromFile(String fileWithDiscount) {
@@ -31,23 +46,46 @@ public class BillGenerator {
             e.printStackTrace();
         }
         return discountMap;
-
     }
 
+    public Map<String, Map<Integer, Float>> getDiscountMap() {
+        return discountMap;
+    }
+
+    public Map<String, Integer> getBillMap() {
+        return billMap;
+    }
+
+    public Float charge() {
+        Float sum = 0.0f;
+
+        for (Map.Entry<String, Integer> entry : billMap.entrySet()) {
+            int k = entry.getValue();
+            for(int i = entry.getValue() ; i > 0; i--) {
+                if(k == 0) {
+                    break;
+                }
+                if(discountMap.get(entry.getKey()).get(i) != null) {
+                    do {
+                        sum += lookForDiscount(entry.getKey(), i);
+                        k = k - i;
+                    }while(k >= i);
+                }
+            }
+        }
+        return Math.round(sum * 100.00f)/100.00f;
+    }
+
+    private Float lookForDiscount(String key, int quantity) {
+        Float sum = 0.0f;
+        sum += discountMap.get(key).get(quantity);
+        return sum;
+    }
 
     public static void main(String[] args) {
         String fileWithDiscount = args[0];
         String fileFromCashDesk = args[1];
         BillGenerator bg = new BillGenerator(fileWithDiscount, fileFromCashDesk);
-        int bill = bg.generateBill();
-        //System.out.println("The total price is:" + bill + "EUR");
-
-    }
-
-    private int generateBill() {
-     //   readDiscountFromFile();
-//        sumBillQuantity();
-//        countTheMoney();
-        return 0;
+        System.out.println("The total price is:" + bg.charge() + " EUR");
     }
 }
